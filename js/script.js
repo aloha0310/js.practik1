4aaadee3a727c83bc4e3735401899688f2d800ad
Alex <03.10alex@mail.ru>
2022-11-02 19:10:52 +0300

    применил циклы

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "use strict";
  
-const numberOfFilms = +prompt('Сколько фильмов уже?', '');
+const numberOfFilms = +prompt('Сколько фильмов уже посмотрели?', '');
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -9,11 +9,31 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Сколько фильмов уже?', ''),
-      b = prompt('На сколько оцените его', ''),
-      c = prompt('Сколько фильмов уже?', ''),
-      d = prompt('На сколько оцените его', '');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмах?', ''),
+          b = prompt('На сколько оцените его', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+    personalMovieDB.movies[a] = b;
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотренно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log ('ошибка');
+}
 
 console.log(personalMovieDB);
+
